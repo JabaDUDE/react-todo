4a468e71c6210f58f855eca2ed9dd1739b5a8616
Dakota <dakotawkeast@gmail.com>
2023-09-25 04:06:24 -0400

    play around with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount((count) => count + 1)
+  }
+
   return (
     <>
       <h1>It Do Be Likie That Sometimes</h1>
+      <div>
+        <input type='text' placeholder='Add Task Here' />
+        <button>Add</button>
+      </div>
+      <div>
+        <h1>You pressed this button {count} times</h1>
+        <button onClick={handleClick}>Press</button>
+      </div>
     </>
   )
 }
