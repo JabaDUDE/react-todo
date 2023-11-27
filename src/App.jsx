3d5d75ddfbb4056595f1da0bd9c47c10ceb857b2
Dakota <dakotawkeast@gmail.com>
2023-11-27 16:38:05 -0500

    update comments and format

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   //   <ToDoList item={todo} />
   // })
 
+  //initial state of newToDo is an empty string, so this function checks to make sure it's not just an empty string after trimming white space from both sides of input, then updates todos when the user hits the Add button. The newToDo is the 'e.target.value' of the input.
   const addTodo = () => {
     if(newTodo.trim() !== ''){
       setTodos([...todos, newTodo])
@@ -19,6 +20,7 @@ function App() {
     }
   }
   // '_' in the filter function is a placeholder since it's not actually beingused to update the todos
+  //function takes the index of the todo that is being 'deleted' and creates an updated todo list
   const deleteTodo = (index) => {
     const updatedTodos = todos.filter((_, i) => i !== index)
     setTodos(updatedTodos)
@@ -32,12 +34,14 @@ function App() {
         <button onClick={addTodo}>Add</button>
       </div>
       <ul>
+      <div>
         {todos.map((todo, index) => (
           <li key={index}>
             {todo}
             <span onClick={() => deleteTodo(index)}>🗑️</span>
           </li>
         ))}
+        </div>
       </ul>
     </>
   )
