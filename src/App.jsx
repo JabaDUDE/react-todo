7215e7696e397b1f7b6d7396743dce864c2e3316
Dakota <dakotawkeast@gmail.com>
2023-12-01 21:36:16 -0500

    worked on styling task list

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ function App() {
   const [todos, setTodos] = useState([])
   const [newTodo, setNewTodo] = useState('')
 
-  // const listItems = todos.map(function(todo){
-  //   <ToDoList item={todo} />
-  // })
-
   //initial state of newToDo is an empty string, so this function checks to make sure it's not just an empty string after trimming white space from both sides of input, then updates todos when the user hits the Add button. The newToDo is the 'e.target.value' of the input.
   const addTodo = () => {
     if(newTodo.trim() !== ''){
@@ -26,8 +22,14 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  //This function should move the todo that is marked 'completed' to a separate list to be grayed out.
+  //TODO: complete this function
+  const completedTodo= (index) => {
+
+  }
+
   return (
-    <div className="flex flex-col justify-center max-w-7xl">
+    <div className="flex flex-col justify-center m-auto max-w-7xl">
     <div className="flex justify-center">
       <h1 className="font-bold tracking-wide text-5xl m-1.5">To Do List</h1>
     </div>
@@ -35,7 +37,7 @@ function App() {
         <UserInput newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
       </div>
       <div>
-        <ToDoList todos={todos} deleteTodo={deleteTodo} />
+        <ToDoList todos={todos} deleteTodo={deleteTodo} completedTodo={completedTodo} />
       </div>
     </div>
   )
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,11 +1,16 @@
 //useState is used as props and passed down here form the parent App.jsx as well as the deleteTodo function
-export default function ToDoList({ todos, deleteTodo }){
+export default function ToDoList({ todos, deleteTodo, completedTodo }){
     return(
-        <ul>
+        <ul className="list-none max-w-5xl">
         {todos.map((todo, index) => (
-          <li key={index}>
-            {todo}
-            <span onClick={() => deleteTodo(index)} className="cursor-pointer">🗑️</span>
+          <li key={index} className="flex m-auto max-w-full">
+            <h3 className="font-bold align-middle">{todo}</h3>
+            <div className="m-2 flex items-center space-x-1.5">
+            <span onClick={() => completedTodo(index)} 
+              className="cursor-pointer">✔️</span>
+            <span onClick={() => deleteTodo(index)} 
+              className="cursor-pointer">🗑️</span>
+           </div>
           </li>
         ))}
       </ul> 
