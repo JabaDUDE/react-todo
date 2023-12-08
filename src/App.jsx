0eac8188555f5221d75979e3eac25ef6136f3932
Dakota <dakotawkeast@gmail.com>
2023-12-07 20:49:31 -0500

    changed input to object id instead of index

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   const [todos, setTodos] = useState([])
   const [newTodo, setNewTodo] = useState('')
 
+  //useState for completed tasks
+  const [completedtodos, setCompletedtodos] = useState([])
+
   //initial state of newToDo is an empty string, so this function checks to make sure it's not just an empty string after trimming white space from both sides of input, then updates todos when the user hits the Add button. The newToDo is the 'e.target.value' of the input.
   const addTodo = () => {
     let id = todos.length + 1
@@ -20,21 +23,27 @@ function App() {
       setNewTodo('')
     }
   }
-  // if(newTodo.trim() !== ''){
-  //   setTodos([...todos, newTodo])
-  //   setNewTodo('')
-  // }
+
   // '_' in the filter function is a placeholder since it's not actually beingused to update the todos
   //function takes the index of the todo that is being 'deleted' and creates an updated todo list
-  const deleteTodo = (index) => {
-    const updatedTodos = todos.filter((_, i) => i !== index)
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((_, i) => i !== id)
     setTodos(updatedTodos)
   }
 
   //This function should move the todo that is marked 'completed' to a separate list to be grayed out.
   //TODO: complete this function
-  const completedTodo= (index) => {
-
+  const completedTodo= (id) => {
+    //grab the task that is being 'completed'
+    const updatedTodos = todos.filter((task, i) => i + 1 == task.id)
+    // //add task to completedtodo usestate with updated completed boolean of true
+    // setCompletedtodos((oldList) => {
+    //   return oldList.map((task) => {
+    //     //set todo with ids
+    //     return task.id == id ? {...task, completed: !task.completed} : task
+    //   })
+    // })
+    console.log(completedtodos);
   }
 
   return (
@@ -46,8 +55,11 @@ function App() {
         <UserInput newTodo={newTodo} setNewTodo={setNewTodo} addTodo={addTodo} />
       </div>
       <div className="max-w-6xl">
-        <ToDoList todos={todos} deleteTodo={deleteTodo} completedTodo={completedTodo} />
+        <ToDoList todos={todos} deleteTodo={deleteTodo} completedTodo={completedTodo}/>
+        <h3>Completed Tasks</h3>
+
         </div>
+
     </div>
   )
 }
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,15 +2,15 @@
 export default function ToDoList({ todos, deleteTodo, completedTodo }){
     return(
         <ul className="list-none max-w-full">
-        {todos.map((todo, index) => (
-          <li key={index} className="flex m-auto justify-between">
+        {todos.map((todo) => (
+          <li key={todo.id} className="flex m-auto justify-between">
             <div>
               <h3 className="font-bold align-middle">{todo.task}</h3>
             </div>
             <div className="m-2 flex items-center space-x-1.5">
-              <span onClick={() => completedTodo(index)} 
+              <span onClick={() => completedTodo(todo.id)} 
                 className="cursor-pointer">✔️</span>
-              <span onClick={() => deleteTodo(index)} 
+              <span onClick={() => deleteTodo(todo.id)} 
                 className="cursor-pointer">🗑️</span>
            </div>
           </li>
